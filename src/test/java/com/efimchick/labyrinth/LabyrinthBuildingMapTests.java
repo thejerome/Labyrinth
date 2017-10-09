01c8d40dd55f11330bf90048b4301ffc49e78f57
thejerome <thejerome@mail.ru>
2017-10-09 15:00:53 +0300

    tests

--- src/test/java/com/efimchick/labyrinth/LabyrinthBuildingMapTests.java
+++ src/test/java/com/efimchick/labyrinth/LabyrinthBuildingMapTests.java
@@ -4,20 +4,25 @@ import com.efimchick.labyrinth.map.Cell;
 import com.efimchick.labyrinth.map.CellType;
 import com.efimchick.labyrinth.map.LabyrinthMap;
 import com.efimchick.labyrinth.map.renderer.LabyrinthMapRenderer;
+import com.efimchick.labyrinth.map.mapsource.CellsCollectionMapSource;
+import com.efimchick.labyrinth.map.mapsource.StringMapSource;
 import com.efimchick.labyrinth.map.position.Position;
 import com.efimchick.labyrinth.map.renderer.RectangularStringLabyrinthMapRenderer;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
- * Created by Evgenii_Efimchik on 05-Oct-17.
+ * Created by Evgenii_Efimchik on 02-Oct-17.
  */
-public class LabyrinthRendererTests {
+public class LabyrinthBuildingMapTests {
 
-    private LabyrinthMap smallQuadMap = new LabyrinthMap(() -> {
+    private LabyrinthMap map1 = new LabyrinthMap(() -> {
         int x = 0, y = 0;
         return Stream.of(
                 new Cell(Position.of(0, 0), CellType.WALL),
@@ -43,9 +48,27 @@ public class LabyrinthRendererTests {
         );
     });
 
-private LabyrinthMap smallRectMap = new LabyrinthMap(() -> {
+    private LabyrinthMap map2 = new LabyrinthMap(() -> {
         int x = 0, y = 0;
         return Stream.of(
+                new Cell(Position.of(x++, y), CellType.WALL),
+                new Cell(Position.of(x++, y), CellType.WALL),
+                new Cell(Position.of(x++, y), CellType.EMPTY),
+                new Cell(Position.of(x++, y), CellType.WALL),
+
+                new Cell(Position.of(x -= 4, y++), CellType.WALL),
+                new Cell(Position.of(x++, y), CellType.EMPTY),
+                new Cell(Position.of(x++, y), CellType.EMPTY),
+                new Cell(Position.of(x++, y), CellType.EMPTY)
+        );
+    });
+
+
+
+    @Test
+    public void mayBeCreatedFromCellCollection(){
+
+        Collection<Cell> cells = Arrays.asList(
                 new Cell(Position.of(0, 0), CellType.WALL),
                 new Cell(Position.of(1, 0), CellType.WALL),
                 new Cell(Position.of(2, 0), CellType.WALL),
@@ -64,40 +87,52 @@ private LabyrinthMap smallRectMap = new LabyrinthMap(() -> {
                 new Cell(Position.of(0, 3), CellType.WALL),
                 new Cell(Position.of(1, 3), CellType.EMPTY),
                 new Cell(Position.of(2, 3), CellType.EMPTY),
-                new Cell(Position.of(3, 3), CellType.EMPTY),
-
-                new Cell(Position.of(0, 4), CellType.WALL),
-                new Cell(Position.of(1, 4), CellType.WALL),
-                new Cell(Position.of(2, 4), CellType.WALL),
-                new Cell(Position.of(3, 4), CellType.WALL)
-
+                new Cell(Position.of(3, 3), CellType.EMPTY)
         );
-    });
 
+        LabyrinthMap map = new LabyrinthMap(new CellsCollectionMapSource(cells));
 
-    @Test
-    public void quadMapMayBeRenderedToString() {
-
-        LabyrinthMapRenderer<String> mapRenderer = new RectangularStringLabyrinthMapRenderer();
-
-        assertEquals("WWWW\n" +
-                        "WEEE\n" +
-                        "WEEE\n" +
-                        "WEEE\n",
-                mapRenderer.render(smallQuadMap).get());
     }
 
 
     @Test
-    public void rectMapMayBeRenderedToString() {
+    public void mayBeCreatedFromString(){
+
+        String mapAsString = "#####\n" +
+                        "#...#\n" +
+                        "#...#\n" +
+                        "#...#";
+
+        LabyrinthMap map = new LabyrinthMap(new StringMapSource(mapAsString));
+
+    }
+
+    @Test
+    public void mayBeCorrectlyCreatedFromString(){
+
+        String mapAsString = "#####\n" +
+                "#...#\n" +
+                "#...#\n" +
+                "#...#";
+
+        LabyrinthMap map = new LabyrinthMap(new StringMapSource(mapAsString));
 
         LabyrinthMapRenderer<String> mapRenderer = new RectangularStringLabyrinthMapRenderer();
+
+        assertTrue(map.iterator().hasNext());
+
+        for (Cell cell : map) {
+            System.out.println(cell);
+        }
 
         assertEquals("WWWWW\n" +
                         "WEEEW\n" +
                         "WEEEW\n" +
                         "WEEEW\n",
-                mapRenderer.render(smallRectMap).get());
+                mapRenderer.render(map).get());
+
+
     }
+
 
 }
